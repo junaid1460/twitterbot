3dec59cfb9dca2f38dc14fcc3ab314f1f40550e9
junaid <junaid1460@gmail.com>
2017-06-03 11:07:15 +0530

    restricting tweets to past 7 days

--- app/router.js
+++ app/router.js
@@ -90,159 +90,183 @@ passport.deserializeUser(function(obj, cb) {
 
 
 
-//some functions will go lengthy and messy 
-//coz javascript's many function are async and that's not good for 
-//our app. serializing is hell :p
-function getData(T,username,cb){
 
-    // callback function for retriving data
-    // method : twitter api function , javascript {}
-    // params : api params
-    // number : pagecount
-    // offsetId : keeping some page count
-    // dataset : final data
-    // callback : function where final data is passed 
-    function obtainTweets (method,params, number, offsetId, dataset, callback) {
-        dataset = dataset || []
-        if (number < 0) return callback(null, dataset)
 
-        return T.get(method, params, (error, data) => {
+function subtract(t = new Date(),t1 = 0){
+    return new Date(
+        t.getFullYear() ,
+        t.getMonth(),
+        t.getDate() - t1,
+        0,0,0,0
+    )
+}
 
-            if (error) return callback(error)
+function lesserThan(range = new Date(),check = new Date()){
+    return check >= range;
+}
 
-            dataset = dataset.concat(data)
-            return obtainTweets(method,params, number-100, offsetId, dataset, callback)
-        })
+
+
+
+// callback function for retriving data
+// method : twitter api function , javascript {}
+// params : api params
+// number : pagecount
+// offsetId : keeping some page count
+// dataset : final data
+// callback : function where final data is passed 
+function obtainTweets (method,params, number, offsetId, dataset, callback) {
+    dataset = dataset || []
+    if (number < 0) return callback(null, dataset)
+
+    return T.get(method, params, (error, data) => {
+
+        if (error) return callback(error)
+
+        dataset = dataset.concat(data)
+        return obtainTweets(method,params, number-100, offsetId, dataset, callback)
+    })
+}
+
+
+
+
+// callback function for retriving friend's /follower's tweets
+//array : array of friends's ids
+// params : api params
+// number : pagecount
+// dataset : final data
+// callback : function where final data is passed 
+function foreach(number,array,params,dataset,callback){
+    dataset = dataset||[]
+    if(number<=0){
+        console.log("calling callback\n")
+        return callback(null,dataset);
     }
-    // callback function for retriving friend's /follower's tweets
-    //array : array of friends's ids
-    // params : api params
-    // number : pagecount
-    // dataset : final data
-    // callback : function where final data is passed 
-    function foreach(number,array,params,dataset,callback){
-        dataset = dataset||[]
-        if(number<=0){
-            console.log("calling callback\n")
-            return callback(null,dataset);
-        }
-        else{
-            console.log(array[number])
-        
-            return T.get('statuses/user_timeline', { user_id : parseInt(array[number]),count : 3200 }, (error, data) => {
-                if(!error)
-                dataset = dataset.concat(data);
-                console.log(dataset.length);
-                return foreach(number-1,array,params,dataset,callback);
-        });
-        }
+    else{
+        console.log(array[number])
+    
+        return T.get('statuses/user_timeline', { user_id : parseInt(array[number]),count : 3200 }, (error, data) => {
+            if(!error)
+            dataset = dataset.concat(data);
+            console.log(dataset.length);
+            return foreach(number-1,array,params,dataset,callback);
+    });
     }
+}
+
+
+ //the data structure for storing necessary information of each tweet
+class Tweet{
+    constructor(user,name,dp,urls,text,date,img){
+        this.user = user;
+        this.urls = urls;
+        this.text = text;
+        this.name = name;
+        this.dp = dp;
+        this.date = date;
+    }
+
+} 
+
+
+function getData(T,username,cb){ //query data from twitter
 
 //here the function flow
-//  1. get current user's tweets
-//  2. get user's friends list/array
-//  3. for each user id get posts related to user id 
-//  4. concatinate dataset
-//  5. extract what you need
-//  6. push it to data base
 
 
-//1
-obtainTweets('statuses/user_timeline',{q:"http:// since:2017-05-23",count : 3000},1,null,null, (error,dataset) =>{
-    console.log(dataset);
-    //2
-    obtainTweets('followers/ids',{ count:3000}, 1, null, null, (error, data) => {
-        //3 and 4
-        foreach(data[0].ids.length -1,data[0].ids,null,dataset,(e,datas) => {
-          var pdata = [];
-          //romove old data if exist. don't want to go out of memory
-          // following things are mongoose stuffs
-          // Now I have all tweets I need
-          // let's do some post processing on the data
-          console.log("processing data");
-          database.Tweet.remove({user : username }).exec((error,res) => {
-            if(error){
-                console.log("database error");
-                cb(null)//so when user trys again i could repeat this process again,
-            }else{
-                    //old data deleted now let's store new
-                    console.log("deleted old data!");
-                    console.log("saving data")
-                    //let's go serially
-                    new Promise(function(res,red){
 
-                        var dt = []; //this var will have processed data
-                        //promise resolver
-                        res(function(){
 
-                            //the data structure for storing necessary information of each tweet
-                            class Tweet{
-                                constructor(user,name,dp,urls,text,date,img){
-                                    this.user = user;
-                                    this.urls = urls;
-                                    this.text = text;
-                                    this.name = name;
-                                    this.dp = dp;
-                                    this.date = date;
-                                }
 
-                            } 
 
-                            //iterate
-                            for(i=0;i<datas.length;i++){
-                                //her I'm filtering tweets
-                                //take only those tweets which has some sort of link in it
-                                if(datas[i].entities.urls.length > 0)
+    //  1. get current user's tweets
+    obtainTweets('statuses/user_timeline',{q:"http:// since:2017-05-23",count : 3000},1,null,null, (error,dataset) =>{
+        //console.log(dataset);
+        //  2. get user's friends list/array
+        obtainTweets('followers/ids',{ count:3000}, 1, null, null, (error, data) => {
+            //  3. for each user id get posts related to user id 
+            //  4. concatinate dataset, the variable data set having current user data is passed to function 
+            //     to concatinate friends' tweets
+            
+            foreach(data[0].ids.length -1,data[0].ids,null,dataset,(e,datas) => {
+            // romove old data if exist. don't want to go out of memory
+            // following things are mongoose stuffs
+            // Now I have all tweets I need
+            // let's do some post processing on the data
+            console.log("processing data");
+            database.Tweet.remove({user : username }).exec((error,res) => {
+                if(error){
+                    // we will be here in case of connection error
+                    console.log("database error");
+                    cb(null)
+                    //when returned is null, the call back function can take some actions
+                    //so when user trys again we can repeat this process again,
+                }else{
+                        //old data deleted now let's store new
+                        console.log("deleted old data!");
+                        console.log("saving data")
+                        //let's go serially
+                        new Promise(function(res,red){
+
+                            var dt = []; //this var will have processed data
+                            //promise resolver
+                            res(function(){
+                                // subtract date by 7
+                                date  = subtract(new Date() - 7);
+                                //iterate thru all received tweets
+                                for(i=0;i<datas.length;i++){
+                                    //  her we are filtering tweets
+                                    //  take only those tweets which has some sort of link in it
+                                    //  and withingthe range from today and (today - 7) ie: within past 7 days 
+                                    //  5. extract what you need
+                                    if(datas[i].entities.urls.length > 0 && lesserThan(date,new Date(datas[i].created_at)))
                                     {
-
-                                        dt.push(new Tweet(datas[i].user.screen_name,
-
-                                            datas[i].user.name,
-                                            datas[i].user.profile_image_url,
-                                            datas[i].entities.urls,
-                                            datas[i].text,
-                                            datas[i].created_at
-                                        ));
+                                            dt.push(
+                                                new Tweet(
+                                                    datas[i].user.screen_name,
+                                                    datas[i].user.name,
+                                                    datas[i].user.profile_image_url,
+                                                    datas[i].entities.urls,
+                                                    datas[i].text,
+                                                    datas[i].created_at
+                                                )
+                                            );
                                     }
                                 }
-                            
-                        }())
+                                
+                            }())
 
-                        // another promise resolver
-                        res(function(){
-                            
-                                database.Tweet({ // the tweet's collection
-                                     user : username,
-                                    tweets :dt      
-                                }).save(function(e,r){
-                                    if(!e){
-                                        console.log("data pulled from twitter and saved successfully!")
-                                        cb(dt);
-                                    } else {
-                                        
-                                        console.log("couldn't pull data!") //logs are always good easy debugging see the message ctrl + f search lmao :p
-                                        cb(null)//so when user trys again i could repeat this process again,
-                                 }
-                             });
-                        }())
-                        //second promise ends here
-                        //seriously saying I have no habit of writing comments
-                        //sorry if there is spelling mistake
-                    })
-                    //end promise
-                    
-                    
-                   
-            }//out from old data's 'else'
-        })// out from datbase delete query
-       
+                            // another promise resolver
+                            // for uploading tweets
+                            res(function(){
+                                    //  6. push it to data base
+                                    database.Tweet({ // the tweet's collection
+                                        user : username,
+                                        tweets :dt      
+                                    }).save(function(e,r){
+                                        if(!e){
+                                            console.log("data pulled from twitter and saved successfully!")
+                                            cb(dt);
+                                        } else {
+                                            
+                                            console.log("couldn't pull data!")
+                                             //logs are always good easy debugging see the message ctrl + f search lmao :p
+                                            cb(null)//so when user trys again i could repeat this process again,
+                                    }
+                                });
+                            }())
+                            //second promise ends here
+                            //seriously saying I have no habit of writing comments
+                            //sorry if there is spelling mistake
+                        })
+                        //end promise
+                    }//out from old data's 'else'
+                })// out from datbase delete query
+        
+            });
+        })
+
     });
-})
-
-});
-
-
-
 }
 
 
